58649ec187c48946b204242cf66369b3ed7273ab
Umanath Jha <jhaumanath93@gmail.com>
2020-07-25 01:09:13 +0530

    Created Interfaces for pet clinic. Closes #1

--- pet-clinic-data/src/main/java/com/learning/spring/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/learning/spring/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.learning.spring.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
